53dcfd5f20b9f623857dd7bad53b62d4ac0524c7
single7troke <alexeydubtsov@gmail.com>
2020-11-12 01:51:24 +0300

    init commit
    added chat server django application
    all methods works well

---
+++ chat_server/chat/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils import timezone
+
+
+class User(models.Model):
+	username = models.CharField(max_length=32, unique=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"Username: {self.username}"
+
+
+class Chat(models.Model):
+	name = models.CharField(max_length=64, unique=True)
+	users = models.ManyToManyField(User)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"Chatname: {self.name}"
+
+
+class Message(models.Model):
+	chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	text = models.CharField(max_length=256)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"message: {self.text}\nuser_id: {self.author}\nchat_id: {self.chat}"
+
